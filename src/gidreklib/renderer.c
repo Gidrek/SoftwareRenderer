3956a0f2b511f09921c9508a58ea54332648b19e
Giovanni Cortes <giovanni@giovannicortes.com>
2025-09-05 09:15:21 -0600

    Created the first renderer implementation

---
+++ src/gidreklib/renderer.c
@@ -0,0 +1,45 @@
+//
+// Created by Giovanni Cortes on 05/09/25.
+//
+
+#include "renderer.h"
+
+
+void renderer_init(Renderer *r, int width, int height) {
+
+    if (!r || width <= 0 || height <= 0) {
+        return;
+    }
+
+    r->width = width;
+    r->height = height;
+    r->framebuffer = (uint32_t *) calloc(width * height, sizeof(uint32_t));
+
+}
+
+void renderer_cleanup(Renderer *r) {
+    if (!r) {
+        return;
+    }
+
+    if (r->framebuffer) {
+        free(r->framebuffer);
+        r->framebuffer = NULL;
+    }
+    r->width = 0;
+    r->height = 0;
+}
+
+void renderer_clear(Renderer *r, uint32_t color) {
+    if (!r || !r->framebuffer) {
+        return;
+    }
+
+    for (int i = 0; i < r->width * r->height; i++) {
+        r->framebuffer[i] = color;
+    }
+}
+
+void renderer_draw_pixel(Renderer *r, int x, int y, uint32_t color) {
+
+}
--- src/gidreklib/renderer.h
+++ src/gidreklib/renderer.h
@@ -5,4 +5,21 @@
 #ifndef INC_3DRENDERER_RENDERER_H
 #define INC_3DRENDERER_RENDERER_H
 
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct {
+    uint32_t *framebuffer;
+    int width;
+    int height;
+} Renderer;
+
+// Lifecycle functions
+void renderer_init(Renderer *r, int width, int height);
+void renderer_cleanup(Renderer *r);
+
+void renderer_clear(Renderer *r, uint32_t color);
+
+void renderer_draw_pixel(Renderer *r, int x, int y, uint32_t color);
+
 #endif//INC_3DRENDERER_RENDERER_H
